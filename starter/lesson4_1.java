3e00c15ac32b84af4970c349d970d6dd03b2ad5f
Anna Knysh <78809979+dragon408@users.noreply.github.com>
2022-06-07 20:43:36 +0300

    Create lesson4_1.java

---
+++ starter/lesson4_1.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int a = 15;
+        String b = "hi";
+        boolean result = b.equals(a);
+        if (result == true){
+            System.out.println(1);
+        }else{
+            System.out.println(2);
+        }
+        //System.out.println(result);
+    }
+}
+
