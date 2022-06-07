1435e2e4cf24c144c0118134a6556041628fa5ff
Anna Knysh <78809979+dragon408@users.noreply.github.com>
2022-06-07 12:20:28 +0300

    Create lesson3_1.java

---
+++ starter/lesson3_1.java
@@ -0,0 +1,15 @@
+package com.company;
+
+import java.util.HashMap;
+
+//comparing variables of different types in java
+
+public class Main {
+    public static void main(String[] args) {
+        int a = 15;
+        String b = "hi";
+        boolean result = b.equals(a);
+        System.out.println(result);
+    }
+}
+
