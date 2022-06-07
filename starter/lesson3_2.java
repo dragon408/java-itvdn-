7182fb28aa303a4c831ceb98cb86c5e29ae294d0
Anna Knysh <78809979+dragon408@users.noreply.github.com>
2022-06-07 12:35:59 +0300

    Create lesson3_2.java

---
+++ starter/lesson3_2.java
@@ -0,0 +1,13 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in); //creating a scanner for keyboard input
+        int a = input.nextInt(); //keyboard input of type int
+        String s = input.nextLine(); //keyboard input of String
+    }
+}
+
