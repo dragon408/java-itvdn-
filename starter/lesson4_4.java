10c31a837fba5bdb9c796d7b8adc0dc2893b6df1
Anna Knysh <78809979+dragon408@users.noreply.github.com>
2022-06-10 14:24:34 +0300

    Create lesson4_4.java

---
+++ starter/lesson4_4.java
@@ -0,0 +1,11 @@
+package com.company;
+//ternary operation
+public class Main {
+
+    public static void main(String[] args) {
+        int a = 1, b = 2, c = 0;
+
+        c = (a < b) ? b : a;
+        System.out.println(c);
+    }
+}
